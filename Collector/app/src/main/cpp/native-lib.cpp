3e423122702fccb77ff3d7801c8b37c1b4fd70c3
zhangyuan <13584851153@163.com>
2022-01-05 14:08:21 +0800

    develop test.

--- Collector/app/src/main/cpp/native-lib.cpp
+++ Collector/app/src/main/cpp/native-lib.cpp
@@ -3,8 +3,8 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_zhy_collector_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello C++";
+    std::string hello = "Hello JNI";
     return env->NewStringUTF(hello.c_str());
 }
--- Collector/app/src/main/java/com/zhy/collector/MainActivity.kt
+++ Collector/app/src/main/java/com/zhy/collector/MainActivity.kt
@@ -3,28 +3,31 @@ package com.zhy.collector
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.zhy.lib_common.utils.LogUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "MainActivity";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("MainActivity", "onCreate ..");
+        LogUtils.i(TAG, "onCreate ..");
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
-        Log.i(TAG, sample_text.text.toString());
+        LogUtils.i(TAG, sample_text.text.toString());
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    private external fun stringFromJNI(): String
+
 
     companion object {
+        val TAG = MainActivity::class.java.simpleName
+
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
